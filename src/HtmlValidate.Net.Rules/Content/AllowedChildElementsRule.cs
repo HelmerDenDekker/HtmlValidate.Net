60a5ce3938e943c7d33ad7d886637177f2653026
Helmer <helmer.dendekker@beterbed.net>
2026-01-19 10:56:10 +0100

    ol ul and menu should only contains li child elements

---
+++ src/HtmlValidate.Net.Rules/Content/AllowedChildElementsRule.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HtmlAgilityPack;
+
+namespace HtmlValidate.Net.Rules.Content;
+
+public class AllowedChildElementsRule : BaseValidator<IEnumerable<HtmlNode>>
+{
+    public override bool IsValid(IEnumerable<HtmlNode> model)
+    {
+        // an ol, ul or menu element should only have li child elements
+
+        model
+            .Where(n => n.Name == "ol" || n.Name == "ul" || n.Name == "menu")
+            .ToList()
+            .ForEach(parent =>
+            {
+                var invalidChildren = parent.ChildNodes
+                    .Where(c => c.NodeType == HtmlNodeType.Element && !c.Name.Equals("li", StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+                if (invalidChildren.Any())
+                {
+                    Results.Add(new ValidationResult($"Only <li> elements are allowed as children of <{parent.Name}>."));
+                }
+            });
+        
+        return Results.Count == 0;
+    }
+}
---
+++ src/Tests/HtmlValidate.Net.Rules.Tests/AllowedChildElementsRuleTests.cs
@@ -0,0 +1,125 @@
+﻿using HtmlAgilityPack;
+using HtmlValidate.Net.Rules.Content;
+
+namespace HtmlValidate.Net.Rules.Tests;
+
+public class AllowedChildElementsRuleTests
+{
+    [Test]
+    public void IsValid_HtmlWithAllowedChildElements_ShouldReturnTrue()
+    {
+        // Arrange
+        var html = "<div><p>Paragraph inside div</p></div>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var validator = new AllowedChildElementsRule();
+        
+        // Act
+        var result = validator.IsValid(doc.DocumentNode.Descendants());
+        
+        // Assert
+        Assert.That(result, Is.True);
+    }
+    
+    [Test]
+    public void IsValid_HtmlWithDisallowedChildElement_ShouldReturnFalse()
+    {
+        // Arrange
+        var html = "<ul><div>Invalid child inside ul</div></ul>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var validator = new AllowedChildElementsRule();
+        
+        // Act
+        var result = validator.IsValid(doc.DocumentNode.Descendants());
+        
+        // Assert
+        Assert.That(result, Is.False);
+        Assert.That(validator.Results.Count, Is.EqualTo(1));
+        Assert.That(validator.Results.First().ErrorMessage, Is.EqualTo("Only <li> elements are allowed as children of <ul>."));
+    }
+    
+    [Test]
+    public void IsValid_HtmlWithAllowedAndDisallowedChildElements_ShouldReturnFalse()
+    {
+        // Arrange
+        var html = "<ul><li>Valid</li><div>Invalid child inside ul</div></ul>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var validator = new AllowedChildElementsRule();
+        
+        // Act
+        var result = validator.IsValid(doc.DocumentNode.Descendants());
+        
+        // Assert
+        Assert.That(result, Is.False);
+        Assert.That(validator.Results.Count, Is.EqualTo(1));
+        Assert.That(validator.Results.First().ErrorMessage, Is.EqualTo("Only <li> elements are allowed as children of <ul>."));
+    }
+    
+    [Test]
+    public void IsValid_HtmlWithDisallowedChildElements_ShouldReturnFalse()
+    {
+        // Arrange
+        var html = "<ul><br><div>Invalid child inside ul</div></ul>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var validator = new AllowedChildElementsRule();
+        
+        // Act
+        var result = validator.IsValid(doc.DocumentNode.Descendants());
+        
+        // Assert
+        Assert.That(result, Is.False);
+        Assert.That(validator.Results.Count, Is.EqualTo(1));
+        Assert.That(validator.Results.First().ErrorMessage, Is.EqualTo("Only <li> elements are allowed as children of <ul>."));
+    }
+    
+    [Test]
+    public void IsValid_HtmlWithSubListsInListItem_ShouldReturnTrue()
+    {
+        // Arrange
+        var html = "<ol> <li> <ol> <li>sub</li> </ol> </li> </ol>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var validator = new AllowedChildElementsRule();
+        
+        // Act
+        var result = validator.IsValid(doc.DocumentNode.Descendants());
+        
+        // Assert
+        Assert.That(result, Is.True);
+    }
+    
+    [Test]
+    public void IsValid_HtmlWithDirectSubLists_ShouldReturnFalse()
+    {
+        // Arrange
+        var html = "<ol> <ol> <li>sub</li> </ol> </ol>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var validator = new AllowedChildElementsRule();
+        
+        // Act
+        var result = validator.IsValid(doc.DocumentNode.Descendants());
+        
+        // Assert
+        Assert.That(result, Is.False);
+    }
+    
+    [Test]
+    public void IsValid_HtmlWithListItemsContainingFlowElements_ShouldReturnTrue()
+    {
+        // Arrange
+        var html = "<ol>\n<li><span>txt</span><br>\n<ol><li>sub</li></ol>\n</li>\n</ol>";
+        var doc = new HtmlDocument();
+        doc.LoadHtml(html);
+        var validator = new AllowedChildElementsRule();
+        
+        // Act
+        var result = validator.IsValid(doc.DocumentNode.Descendants());
+        
+        // Assert
+        Assert.That(result, Is.True);
+    }
+}
