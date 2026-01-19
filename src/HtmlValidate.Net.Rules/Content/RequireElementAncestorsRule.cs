a957e57fce4a61e6f9c2604072fc4e69a3c6651b
Helmer <helmer.dendekker@beterbed.net>
2026-01-19 09:09:55 +0100

    list item li should have ol, ul or menu ancestor

---
+++ src/HtmlValidate.Net.Rules/Content/RequireElementAncestorsRule.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using HtmlAgilityPack;
+
+namespace HtmlValidate.Net.Rules.Content;
+
+public class RequireElementAncestorsRule : BaseValidator<HtmlDocument>
+{
+    public override bool IsValid(HtmlDocument model)
+    {
+        // start with documentNode here
+
+        // an li child element, should have an <ul>, <ol> or <menu> direct parent
+
+        model.DocumentNode.Descendants()
+            .Where(n => n.Name == "li")
+            .ToList()
+            .ForEach(li =>
+            {
+                var parent = li.ParentNode;
+                var hasValidAncestor = parent.Name.Equals("ul", StringComparison.OrdinalIgnoreCase) ||
+                                       parent.Name.Equals("ol", StringComparison.OrdinalIgnoreCase) ||
+                                       parent.Name.Equals("menu", StringComparison.OrdinalIgnoreCase);
+                if (!hasValidAncestor)
+                    Results.Add(new ValidationResult("<li> element must have a <ul>, <ol> or <menu> ancestor."));
+            });
+        
+        return Results.Count == 0;
+    }
+}
